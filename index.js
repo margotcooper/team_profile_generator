217e1eb2af600aaaaa6cacddc3503d39590921fe
Margot Cooper Gruen <margotgruen@gmail.com>
2022-08-23 12:31:27 -0700

    first push. added all files in structure given.

---
+++ index.js
@@ -0,0 +1,27 @@
+class Manager {
+  constructor(name, employeeid, email, officenumber) {
+    this.name = name;
+    this.employeeid = employeeid;
+    this.email = email;
+    this.officenumber = officenumber;
+    this.newManager = () => {
+        if (this.Manager)
+    }
+  }
+}
+class Engineer {
+  constructor(name, id, email, githubusername) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.githubusername = githubusername;
+  }
+}
+class Intern {
+  constructor(name, ID, email, school) {
+    this.name = name;
+    this.ID = ID;
+    this.email = email;
+    this.school = school;
+  }
+}

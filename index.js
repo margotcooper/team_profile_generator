076636f1ca35e2084c80358de241adadc491da58
Margot Cooper Gruen <margotgruen@gmail.com>
2022-08-29 18:22:59 -0700

    added inquirer prompts in index.js for Engineer and Intern per Matt

--- index.js
+++ index.js
@@ -1,31 +1,103 @@
 const inquirer = require("inquirer");
 
-const prompt = inquirer.prompt([]).then(() => {});
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your (the manager) name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is your employee ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+      },
+  ])
+  .then((manager) => {
+    console.table(manager);
+    //adding a card
+    //create a card for each employee
+  });
 
-// const Manager = new Employee () {
-//   constructor(name, employeeid, email, officenumber) {
-//     this.name = name;
-//     this.employeeid = employeeid;
-//     this.email = email;
-//     this.officenumber = officenumber;
-//     this.newManager = () => {
-//         if (this.Manager)
-//     }
-//   }
-// }
-// class Engineer {
-//   constructor(name, id, email, githubusername) {
-//     this.name = name;
-//     this.id = id;
-//     this.email = email;
-//     this.githubusername = githubusername;
-//   }
-// }
-// class Intern {
-//   constructor(name, ID, email, school) {
-//     this.name = name;
-//     this.ID = ID;
-//     this.email = email;
-//     this.school = school;
-//   }
-// }
+  inquirer.prompt([
+    //put engineer questions including duplicating manager questions
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+          type: "input",
+          name: "officeNumber",
+          message: "What is your office number?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+          },
+  ]);
+
+  inquirer.prompt([
+    //put intern questions including duplicating manager questions
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+          type: "input",
+          name: "officeNumber",
+          message: "What is your office number?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the name of your school?",
+          },
+  ]);
+  inquirer.prompt([
+  {
+    type: "list",
+    name: "addEmployee",
+    message: "What type of employee would you like to add next?",
+    choices: ["Engineer", "Intern"],
+  }]);
+  if (addEmployee) = "Engineer";
+  .then
+
+//make choice depending on user answer, defines what choice to make next (intern or engineer)
+//if, then, new functions
+
+//do not use employee class, that was just a helper/base class.
+//use manager, engineer, intern classes
